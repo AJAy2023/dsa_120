30af56414d79247a50e2806468cdca01cc1c7d2b
Ajay Jadhav <115634272+AJAy2023@users.noreply.github.com>
2024-12-17 14:40:37 +0000

    finding max number in an array

---
+++ array_all/max_array.java
@@ -0,0 +1,31 @@
+package array_all;
+
+import java.util.Scanner;
+ class max_array {
+
+   public static void main(String[] args) {
+        // taking input from the user
+        int n;
+        System.out.println("enter the size of array");
+        Scanner r = new Scanner (System.in);
+        n  = r.nextInt();
+        int [] array = new int[n];
+        for(int i=0; i<n; i++)
+        {
+            array[i] = r.nextInt();
+        }
+            int max = array[0];
+           
+            for(int i=0; i<n; i++)
+            {
+                if(max<array[i])
+                {
+                    max = array[i];
+                  
+                }
+              
+            }
+            System.out.println(max  + " is a largest number of an array");
+    }
+}
+
