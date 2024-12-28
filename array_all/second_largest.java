6c785699ff32cf0f2d7dd82063164d005c05c916
Ajay Jadhav <115634272+AJAy2023@users.noreply.github.com>
2024-12-28 15:11:42 +0000

    better & optimal approach

--- array_all/second_largest.java
+++ array_all/second_largest.java
@@ -3,40 +3,84 @@ package array_all;
 import java.util.Scanner;
 
 //Q1. second largest number in an array in  this quetion we are solving same problem in an two way one is better and optimal approach 
+
+
+
 // 1. better approach 
+// public class second_largest {
+//         public static void main(String[] args) {
+//         Scanner scn  =  new Scanner(System.in);
+//         System.out.println("enter the array size");
+//         int n  = scn.nextInt();
+//         System.out.println("enter the array elements");
+//         int [] array = new int[n];
+//         for(int i=0; i<n; i++)
+//         {
+//             array[i] = scn.nextInt();
+//         }
+//         scn.close();
+//         System.out.println(functionlargest(array));
+//     }
+
+//     public static int functionlargest(int[] array)
+//     {
+//         int max= array[0];
+//         int second_larg = -1;
+//         for(int i=0; i<array.length; i++)
+//         {
+//             if(array[i]>max)
+//             {
+//                 max = array[i];
+//             }
+//         }
+//         for(int i=0; i<array.length; i++)
+//         {
+//             if(array[i]>second_larg && array[i]<max)
+//             {
+//                 second_larg = array[i];
+//             }
+//         }
+//         return second_larg;
+//     }
+// }
+
+
+
+
+// optimal one 
 public class second_largest {
-        public static void main(String[] args) {
-        Scanner scn  =  new Scanner(System.in);
-        System.out.println("enter the array size");
-        int n  = scn.nextInt();
-        System.out.println("enter the array elements");
-        int [] array = new int[n];
-        for(int i=0; i<n; i++)
+
+    public static void main(String[] args) {
+        
+        int [] array  = {2,4,5,7,8,1};
+
+        int result  = functionlargest(array);
+        if(result == -1)
         {
-            array[i] = scn.nextInt();
+            System.out.println("no  largest element found");
+        } else{
+            System.out.println("The second largest number is : " +  result);
         }
-        scn.close();
-        System.out.println(functionlargest(array));
+
     }
 
-    public static int functionlargest(int[] array)
+    public static int functionlargest(int[]array)
     {
-        int max= array[0];
-        int second_larg = -1;
+        int largest = Integer.MIN_VALUE;
+        int slarges = Integer.MIN_VALUE;
         for(int i=0; i<array.length; i++)
         {
-            if(array[i]>max)
+            if(array[i] > largest)
             {
-                max = array[i];
-            }
-        }
-        for(int i=0; i<array.length; i++)
-        {
-            if(array[i]>second_larg && array[i]<max)
+                slarges = largest;
+                largest = array[i];
+            }else if(array[i]>slarges && array[i]<largest)
             {
-                second_larg = array[i];
+                slarges = array[i];
             }
+          
         }
-        return second_larg;
+        return slarges == Integer.MIN_VALUE ? -1 : slarges;
     }
 }
+
