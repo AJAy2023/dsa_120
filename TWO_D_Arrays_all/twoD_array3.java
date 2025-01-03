76a4a3a1c7b40f2e7f2931a8a4a073676edc6c95
Ajay Jadhav <115634272+AJAy2023@users.noreply.github.com>
2025-01-03 20:25:36 +0000

    Print col wise integers

---
+++ TWO_D_Arrays_all/twoD_array3.java
@@ -0,0 +1,36 @@
+package TWO_D_Arrays_all;
+
+//Q.  Given a 2D array, Print col wise integers.
+import java.util.Scanner;
+
+public class twoD_array3 {
+
+    public static void main(String[] args) {
+        
+        Scanner scn  = new Scanner(System.in);
+        int n  = scn.nextInt();
+        int m  = scn.nextInt();
+        // takign array 
+        int [][] arr = new int[n][m];
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j<m; j++)
+            {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        System.out.println("-----------");
+        for(int j=0; j<arr[0].length; j++)
+        {
+            for(int i=0; i<arr.length; i++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+}
