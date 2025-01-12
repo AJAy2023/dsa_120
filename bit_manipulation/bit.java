1c99f9d48c3a8d781fe65e1c6f81d07c2f64ba9b
Ajay <ajayjadhav0913@gmail.com>
2025-01-12 09:53:35 +0530

    bit manipulation_01

---
+++ bit_manipulation/bit.java
@@ -0,0 +1,30 @@
+//  Take as input a number n. Determine whether it is even or odd. If it is even, 
+// print "even" otherwise print "odd".
+package  dsa_120.bit_manipulation;
+import java.util.*;
+public class bit {
+
+    public static void main(String[] args) {
+        // Create a Scanner object for input
+        Scanner scn = new Scanner(System.in);
+        
+        // Read the input number
+        int n = scn.nextInt();
+        
+        // Determine and print whether the number is even or odd
+        if (isEven(n)) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+        
+        // Close the scanner
+        scn.close();
+    }
+
+    // Method to check if a number is even
+    public static boolean isEven(int n) {
+        // Check the least significant bit using bitwise AND
+        return (n & 1) == 0;
+    }
+}
