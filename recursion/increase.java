b88c4a67bc55fa61631ff6139003c4e27d702882
Ajay <ajayjadhav0913@gmail.com>
2025-01-15 23:29:52 +0530

    Print numbers from 1 to N in N lines - Recursion

--- recursion/increase.java
+++ recursion/increase.java
@@ -1,5 +1,27 @@
 package recursion;
 
+import java.util.Scanner;
+
+//Q1. Print numbers from 1 to N in N lines
+// simple input :- 4 output => 1 2 3 4 
 public class increase {
-    
+    public static void main(String[] args) {
+        Scanner scn  = new Scanner(System.in);
+        int n  = scn.nextInt();
+
+         pritIncr(n);
+           
+    }
+
+    public static int pritIncr(int n)
+    {
+        if( n<=0)
+        {
+            return 1;
+        }
+        pritIncr(n-1);
+        System.out.println(n);
+        return n;
+    }
+
 }
