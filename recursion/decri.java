10c1ebba24a38eef1a7aa5968b32840b7337081c
Ajay <ajayjadhav0913@gmail.com>
2025-01-17 00:09:47 +0530

    Print Numbers from N to 1 - Recursion

--- recursion/decri.java
+++ recursion/decri.java
@@ -6,4 +6,19 @@ import java.util.Scanner;
 //  input 3 : - 3 2 1 
 public class decri {
     
+    public static void main(String[] args) {
+        
+        Scanner scn  = new Scanner(System.in);
+        int n  = scn.nextInt();
+         DecriInt(n);
+    }
+    public static void DecriInt(int n)
+    {
+        if(n == 0)
+        {
+            return ;
+        }
+        System.out.println(n + " ");
+        DecriInt(n -1);
+    }
 }
