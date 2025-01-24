d34e72aaefab4598070fc8fbf1065eb19c589c2e
Ajay <ajayjadhav0913@gmail.com>
2025-01-24 20:33:09 +0530

    sorting and remove the element for unsorted array and make acount of it

---
+++ sorting_programs/sorting_removal.java
@@ -0,0 +1,28 @@
+package sorting_programs;
+import java.util.*;
+public class sorting_removal {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        
+        System.out.println(Sortofarray(arr));
+    }
+
+    public static int Sortofarray(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        int ans = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i] * (n - i);  // Fixed the formula
+            ans = ans + temp;
+        }
+        
+        return ans;
+    }
+}
